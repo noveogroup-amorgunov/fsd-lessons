43fe50462c77829d18a60a98b8729f7c36d79531
amorgunov <noveogroupamorgunov@gmail.com>
2025-04-30 14:06:33 +0300

    Pass trigger for dialogs

--- packages/react-with-user-session/src/entities/session/ui/LogoutButton.tsx
+++ packages/react-with-user-session/src/entities/session/ui/LogoutButton.tsx
@@ -2,15 +2,18 @@ import { useDialog } from '@monorepo/react-core/services/dialog-manager'
 import { ConfirmDialog } from '@monorepo/react-core/ui'
 import { wrap } from '@reatom/core'
 import { reatomComponent } from '@reatom/react'
+import { useRef } from 'react'
 import { logout } from '../model/actions'
 
 export const LogoutButton = reatomComponent(() => {
   const { show: showAlert, hide: hideAlert } = useDialog(ConfirmDialog)
+  const buttonRef = useRef<HTMLButtonElement>(null)
 
   const onLogout = wrap(async () => {
     await showAlert({
       title: 'Logout',
       message: 'Are you sure you want to logout?',
+      triggerRef: buttonRef,
       onConfirm: wrap(async () => {
         await logout()
         hideAlert()
@@ -21,5 +24,5 @@ export const LogoutButton = reatomComponent(() => {
     })
   })
 
-  return <button onClick={onLogout}>Logout</button>
+  return <button ref={buttonRef} onClick={onLogout}>Logout</button>
 })
